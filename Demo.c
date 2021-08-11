a07771088b0d170c52c8d420356fcc9678702a8a
Aman <amantiwari8861@gmail.com>
2021-08-11 17:53:16 +0530

    2nd class C

---
+++ Demo.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+//standard input output header file (pre-processor directives)
+//directives -> folder
+int main()  //starting point of a program
+{
+	printf("hello world!");
+	// ; end of line
+	return 0;//ending point of a program 
+}
---
+++ abc2.c
@@ -0,0 +1,25 @@
+#include<stdio.h> //pre-processor directives(folder)
+#include<conio.h> //console input output headerfile
+int main()
+{
+	printf("hello world! \n");
+	printf(" my name is Aman ");
+	//variables : it is a type of container which stores our daTA
+	//at any specific location
+	//	syntax: data_type variable_name=value;
+	int aman=10,aman1=20,result;
+	result=aman+aman1; 
+	printf("\n the result is %d \n anything. ",result);
+	// format specifier : eg. %d,%f etc	
+	getch();//to hold the output screen
+	return 0;
+}
+// text editor -> notepad,sublime,notepad++,vs code etc
+//IDE -> integrated development environment 
+// IDE examples -> turbo c/c++ ,dev cpp/c++,code blocks etc
+// compiler -> which converts our source code to binary code(0,1)
+// compiler example -> gcc,bcc
+// IDE = textEditor + compiler + intellisence
+// 11-12
+
+
