4a05551065f2842946b260bf6b3a508e34f48a05
Aman <amantiwari8861@gmail.com>
2021-08-16 12:54:24 +0530

    basic structure

---
+++ basicStructure.c
@@ -0,0 +1,7 @@
+#include<stdio.h> //header files
+int main() //starting point of a program
+{//body opening of main function
+	//code or statements
+	return 0;//ending point of a program (it also passes msg to 
+	//operating system that program succesfully executed)
+}//body close of main function
