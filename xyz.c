a9f051385b9d1e5d296d78f068c94ddb74c98228
Aman <amantiwari8861@gmail.com>
2021-08-20 18:31:38 +0530

    revision

---
+++ xyz.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+//standard input output header file (pre-processor directives)
+//directives -> folder
+int main()  //starting point of a program
+{ 	//opening of main function body 
+	printf("hello my name is aman !");
+	// ; end of line
+	return 0;//ending point of a program 
+}
+//single line comment 
+/*
+	multiple
+	line
+	comment
+*/
