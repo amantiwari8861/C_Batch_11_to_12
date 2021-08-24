a443878f570397a67fbc46a76456a428d8076214
Aman <amantiwari8861@gmail.com>
2021-08-24 17:03:51 +0530

    bitwise operators  done

---
+++ leftShift.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	
+//	int num=10;
+//	printf(" %d ",--num);
+//	printf("%d \t ",16<<2);//64
+//	printf("%d \t ",16>>2);//4
+//	printf("%d \t ",16>>3);//2
+//	printf("%d \t ",16<<3);//128
+	
+	printf(" %d ",23&19);//19
+	printf("%d",23|19);//23
+	
+	return 0;
+}
