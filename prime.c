4ca1fcea9cf332ab57eae770e4ddfb41d3fd2044
Aman <amantiwari8861@gmail.com>
2021-09-09 13:07:22 +0530

    prime no. done

---
+++ prime.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    // A prime number is a positive integer that is divisible only by 1 and itself. 
+    // For example: 2, 3, 5, 7, 11, 13, 17 
+    int num,temp=0;
+   printf("enter the no. \n");
+   scanf("%d",&num);
+
+   for (int i = 2; i < num; i++)
+   {
+    //    printf("the value of num is %d \n",i);
+
+        if (num%i==0)
+        {
+           temp=1; 
+        }
+    }
+    if (temp==1)
+    {
+        printf("THIS IS NOT A PRIME NO.");
+    }
+    else
+    {
+        printf("this is a prime no.");
+    }
+    return 0;
+}
