515207cdec37d2a701af1097ee418f83021290ee
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-01-10 13:02:24 +0530

    linear search

--- 6-Arrays_&_multidimension_Arrays/2DArray.c
+++ 6-Arrays_&_multidimension_Arrays/2DArray.c
@@ -11,7 +11,7 @@ int main()
             scanf("%d",&matr[i][j]);
         }
     }
-     for(int i=0;i<2;i++)
+    for(int i=0;i<2;i++)
     {
         for(int j=0;j<3;j++)
         {
--- 6-Arrays_&_multidimension_Arrays/linearSearch.c
+++ 6-Arrays_&_multidimension_Arrays/linearSearch.c
@@ -1,29 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int arr[10],find;
+    int arr[10], find, flag = 0;
     printf("enter the array elements \n");
-    
+
     for (int j = 0; j < 10; j++)
     {
-        scanf("%d",&arr[j]);
+        scanf("%d", &arr[j]);
     }
     printf("enter the element to find out from array \n");
-    scanf("%d",&find);
-    
+    scanf("%d", &find);
+
     for (int i = 0; i < 10; i++)
     {
-        if (arr[i]==find)
+        if (arr[i] == find)
         {
-            printf("found at position %d \n",i);
-        }
-        else
-        {
-            printf("not found at position %d \n",i);
+            printf("found at position %d \n", i);
+            flag = 1;
         }
     }
-    
+
+    if (!flag)
+    {
+        printf("not found in array \n");
+    }
 
     return 0;
 }
