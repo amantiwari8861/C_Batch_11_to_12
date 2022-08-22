6bc1684bdc285953dd79e758496b2403b780342c
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-08-22 12:31:54 +0530

    added codes

---
+++ 08-Function/cone.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+float TSA(float);
+float CSA(float);
+float volume(float);
+float height(float);
+
+const float pi=3.14;
+
+int main()
+{
+    float r;
+    printf("enter the radius:");
+    scanf("%f",&r);
+
+    printf(" TSA is %f \n",TSA(r));
+    printf(" CSA is %f \n",CSA(r));
+    printf(" volume is %f \n",volume(r));
+    printf(" height is %f \n",height(r));
+
+    return 0;
+}
+float TSA(float r)
+{
+    printf("\n--------------Finding TSA-----------------------\n");
+    float l;
+    printf("enter l :");
+    scanf("%f",&l);
+    return pi*r*(l+r);
+}
+float CSA(float r)
+{
+    printf("\n--------------Finding CSA-----------------------\n");
+    float l;
+    printf("enter l :");
+    scanf("%f",&l);
+    return pi*r*l;
+}
+float volume(float r)
+{
+    printf("\n--------------Finding volume-----------------------\n");
+    float h;
+    printf("enter h :");
+    scanf("%f",&h);
+    return 1.0/3.0*pi*r*r*h;
+}
+float height(float r)
+{
+    printf("\n--------------Finding height-----------------------\n");
+    float l;
+    printf("enter l :");
+    scanf("%f",&l);
+    return sqrt(pow(l,2)-pow(r,2));
+}
--- extra/fxnDemo.c
+++ extra/fxnDemo.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-int isArmstrong(int);
 int isPalindrome(int);
 int reverse(int);
 int main()
@@ -7,29 +6,11 @@ int main()
     int num;
     printf("enter number :");
     scanf("%d",&num);
-
-    isArmstrong(num);
+    printf("the reverse is %d \n",reverse(num));
+    isPalindrome(num);    
     return 0;
 }
-int isArmstrong(int num)
-{
-    int rem,sum=0;
-    int original=num;
-    while (num>0)
-    {
-        rem = num%10 ; 
-        sum = sum + rem*rem*rem;
-        num=num/10 ;
-    }
-    if (sum == original)
-    {
-        printf(" %d is armstrong \n",original);
-    }
-    else
-    {
-        printf(" %d is not an armstrong \n",original);
-    }
-}
+
 int reverse(int num)
 {
     int rem,rev=0;
@@ -40,7 +21,7 @@ int reverse(int num)
         rev = rev*10 + rem;
         num=num/10 ;
     }
-    printf("the reverse is %d \n",rev);
+    // printf("the reverse is %d \n",rev);
     return rev;
 }
 int isPalindrome(int num)
