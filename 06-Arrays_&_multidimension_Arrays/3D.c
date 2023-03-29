bfbea6016f9e1f2c04fe98ef068f60b1582b9a92
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-03-29 17:37:41 +0530

    user defined 3d array

--- 06-Arrays_&_multidimension_Arrays/3D.c
+++ 06-Arrays_&_multidimension_Arrays/3D.c
@@ -2,12 +2,25 @@
 int main()
 {
 	// initializing the 3-dimensional array
-	int x[2][3][2] =
+	// int x[2][3][2] =
+	// {
+	// 	{ {10,16}, {23,37}, {4,5} },
+	// 	{ {6,7}, {8,9}, {180,11} }
+	// };
+	int x[2][3][2];
+	printf("Enter the elements in 3d array\n");
+	for (int i = 0; i < 2; i++)
 	{
-		{ {10,16}, {23,37}, {4,5} },
-		{ {6,7}, {8,9}, {180,11} }
-	};
-
+		for (int j = 0; j < 3; j++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				scanf("%d",&x[i][j][k]);
+			}
+		}
+	}
+	
+	printf("The 3d array is :\n");
 	// output each element's value
 	for (int i = 0; i < 2; ++i)
 	{
