a54158e429cdfcea8ea7d19f05dae6bfa8496c1a
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-05-23 17:55:44 +0530

    typecasting

--- 04-Type_casting_&_ASCII/typecast.c
+++ 04-Type_casting_&_ASCII/typecast.c
@@ -5,8 +5,8 @@ int main()
     // int num=50;
     // float f;
     // f=num;//implicit type casting
-    // // // or
-    // f=(float)num;//compiler implicitly doing this to num
+    // // or
+    // // f=(float)num;//compiler implicitly doing this to num
     // f=f+0.2;
     // printf("the value of f is %f\n",f);
 
@@ -19,14 +19,14 @@ int main()
     // printf("the value of num2 is %d\n",num2);
 
     //3.casting int to char
-    // int num3=65;
+    // int num3=38;
     // char ch2;
     // ch2=num3;
     // // ch2=(char)num3;//compiler automatically doing this
     // printf("the value of ch2 is %c\n",ch2);
 
     //4.casting char to int
-    char ch3='A';
+    char ch3='B';
     int num4;
     num4=ch3;
     // num4=(int)ch3;//compiler automatically doing this
