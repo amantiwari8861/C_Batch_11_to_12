283a9a16d129ecebc0c039e10b818f7f906163a0
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-05-31 11:57:58 +0530

    loops exercise

---
+++ extra/Q10.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int num,len,evenSum,oddSum,rem;
+    len=evenSum=oddSum=0;
+    printf("enter the number:");
+    scanf("%d",&num);
+
+    int temp,temp2,temp3;
+    temp=temp2=temp3=num;
+    while (temp>0)
+    {
+        len++;
+        temp=temp/10;
+    }
+    
+    printf("the length is %d \n",len);
+
+    while (temp2>0)
+    {
+        rem=temp2%10;
+        if (rem%2==0)
+        {
+            evenSum=evenSum+rem;
+        }
+        else
+        {
+            oddSum=oddSum+rem;
+        }
+        temp2=temp2/10;
+    }
+    printf(" odd sum=%d even sum=%d\n",oddSum,evenSum);
+    
+    printf("Fibonnaci Series : %d %d ",oddSum,evenSum);
+
+    int t1=oddSum,t2=evenSum,t3;
+    for (int i = 0; i < len-2; i++)
+    {
+        t3=t1+t2;
+        printf(" %d ",t3);
+        t1=t2;
+        t2=t3;
+    }
+    
+    return 0;
+}
