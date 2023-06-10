13be48ea930711fa862d36ad92f062efbfde9621
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-06-10 18:45:57 +0530

    basic changes

---
+++ 04-Type_casting_&_ASCII/testASCII.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    for (int i = 0; i < 256; i++)
+    {
+        printf(" %c \n",i);
+    }
+    
+    return 0;
+}
--- 11-Strings/nameprinting4.c
+++ 11-Strings/nameprinting4.c
@@ -4,8 +4,8 @@ int main()
 	//String means sequence of characters 
 	char name[20];
 	printf("enter name :");
-	// scanf("%s",&name);
-	// scanf("%[^\n]s",&name);//to read the string(including space) till enter is pressed 
+	scanf("%s",&name);
+	scanf("%[^\n]s",&name);//to read the string(including space) till enter is pressed 
 	// scanf("%[^\n]s%*c",name);//to read the string(including space) till enter is pressed 
 
 	printf("the name is %s \n",name);
