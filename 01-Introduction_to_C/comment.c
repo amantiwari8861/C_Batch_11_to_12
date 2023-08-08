c7776551b760423b1dfada5ae682138ac289165e
Aman <amantiwari8861@gmail.com>
2023-08-08 13:11:33 +0530

    minor changes

--- 01-Introduction_to_C/comment.c
+++ 01-Introduction_to_C/comment.c
@@ -4,10 +4,10 @@ int main()
     // printf("hello aman sir");
     // printf("hello aman sir");
 
-    // printf("hello aman sir \n");
-    // printf("hello aman sir ");
+    // printf("\n\nhello aman sir \n");
+    // printf("hello aman sir \n\n");
 
-    printf("hello \n aman sir \n");
+    // printf("hello \n aman sir \n");
 
     // printf("hello aman sir 2.0\n");
     // printf("hello aman sir 3.0\n"); // double forward slash means single line
@@ -17,9 +17,9 @@ int main()
     printf("hello aman sir 3.0\n");
     printf("hello aman sir 3.0\n");
     printf("hello aman sir 3.0\n"); // multiple line comment
+    printf("hello aman sir 3.0\n");
     printf("hello aman sir 3.0\n");*/
-    // printf("hello aman sir 3.0\n");
-    
-    // printf("hello aman sir 3.0\n");
+    printf("hello aman sir 3.0\n");
+
     return 0;
 }
